156405aa832cbbb35fe2e74740ac41710669ce8f
Carol Alfonso <calfonsov@unal.edu.co>
2025-10-08 07:30:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from pokemon import Pokemon
 
 if __name__ == "__main__":
-    bulbasaur = Pokemon(
+    bulbasaur_lv1 = Pokemon(
         "bulbasaur",
         1,
         "grass",
         "blue",
-        "male"
+        "male",
+        level=1
     )
-    print(bulbasaur)
-    bulbasaur.attack()
+
+    bulbasaur_lv10 = Pokemon(
+        "bulbasaur",
+        1,
+        "grass",
+        "green",
+        "male",
+        level=10
+    )
+
+    print("Nivel 1:", bulbasaur_lv1)
+    bulbasaur_lv1.attack()
+    print("Nivel 10:", bulbasaur_lv10)
